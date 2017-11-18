762d24511afcdbc85480635d98b6cad98fc93513
Abraham Hurtado <arhc.hc@gmail.com>
2017-11-18 12:07:48 -0700

    Modelos final final

--- app/src/main/java/com/example/abraham/googlebooks/Models/ImageLinks.kt
+++ app/src/main/java/com/example/abraham/googlebooks/Models/ImageLinks.kt
@@ -7,6 +7,10 @@ import com.google.gson.annotations.SerializedName
  */
 data class ImageLinks (
      @SerializedName("smallThumbnail") val smallThumbnail: String,
-     @SerializedName("thumbnail") val thumbnail : String
+     @SerializedName("thumbnail") val thumbnail : String,
+     @SerializedName("small") val small : String,
+     @SerializedName("medium") val medium : String,
+     @SerializedName("large") val large : String,
+     @SerializedName("extraLarge") val extraLarge : String
 ) {
 }
