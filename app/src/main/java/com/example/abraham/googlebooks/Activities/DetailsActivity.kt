9475e046bf6f640c7059ae31647337dd79d99cce
Germán Alberto Verdugo <gava96.g@gmail.com>
2017-11-18 13:58:08 -0700

    implementando el DetailsActivity

--- app/src/main/java/com/example/abraham/googlebooks/Activities/DetailsActivity.kt
+++ app/src/main/java/com/example/abraham/googlebooks/Activities/DetailsActivity.kt
@@ -2,8 +2,16 @@ package com.example.abraham.googlebooks.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.abraham.googlebooks.Models.VolumeInfo
 import com.example.abraham.googlebooks.R
+import com.example.abraham.googlebooks.Service.GoogleBookClient
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.handset_layout.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -11,5 +19,27 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.googleapis.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val googleBookClient = retrofit.create(GoogleBookClient::class.java)
+
+        googleBookClient.getBookById(intent.getStringExtra("id")).enqueue(
+                object : Callback<VolumeInfo> {
+                    override fun onResponse(call: Call<VolumeInfo>?, response: Response<VolumeInfo>?) {
+                        Picasso.with(applicationContext).load(response?.body()?.imageLinks?.large).into(bookImage)
+                        publishDate.text = response?.body()?.publishedDate
+                        authors.text = response?.body()?.authors.toString()
+                        description.text = response?.body()?.description
+                    }
+
+                    override fun onFailure(call: Call<VolumeInfo>?, t: Throwable?) {
+                        // oh shit waddup
+                    }
+
+                }
+        )
     }
 }
--- app/src/main/java/com/example/abraham/googlebooks/Service/GoogleBookClient.kt
+++ app/src/main/java/com/example/abraham/googlebooks/Service/GoogleBookClient.kt
@@ -1,12 +1,17 @@
 package com.example.abraham.googlebooks.Service
 
+import com.example.abraham.googlebooks.Models.VolumeInfo
 import com.example.abraham.googlebooks.Models.VolumesSearchResult
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface GoogleBookClient {
     @GET("/books/v1/volumes?q=android&startIndex=0&maxResults=10")
     fun getBooks(): Call<VolumesSearchResult>
+
+    @GET("/books/v1/volumes/{volumeId}")
+    fun getBookById(@Path("volumeId") id: String): Call<VolumeInfo>
 }
 
