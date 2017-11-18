44b07d2b3ac11de1c9f0291f7ae9830554caa162
xDidier901 <xdidier901@gmail.com>
2017-11-18 11:49:41 -0700

    otro cambio chiquito

--- app/src/main/java/com/example/abraham/googlebooks/MainActivity.kt
+++ app/src/main/java/com/example/abraham/googlebooks/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
     }
+
 }
